d81208160283509245c4f0f4780900733f67475c
kahqgc <kahqgc@gmail.com>
2025-03-11 16:31:25 -0500

    debug common errors

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,18 +24,19 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp");
 
-// What’s Wrong?
+
+// What’s Wrong?: added end quotes, end parenthesis, and semicolon
 
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
-  let doubled = numbers[i] * 2;
+  let doubled = numbers[i]* 2;
   console.log(doubled);
 }
 
@@ -51,10 +52,10 @@ function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
